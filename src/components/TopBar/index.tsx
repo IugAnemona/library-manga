2c8016879e7867c2d12c19dfbf1d14632524d4f9
Guilherme Alves <guilhermealvescoc@gmail.com>
2024-05-25 08:10:49 -0300

    feat: add TopBar component

---
+++ src/components/TopBar/index.tsx
@@ -0,0 +1,50 @@
+export default function TopBar() {
+  return (
+    <header className="fixed top-0 left-0 right-0 bg-gray-800 text-white">
+      <nav className="container mx-auto flex items-center justify-between p-4">
+        <div className="text-xl font-bold">
+          <a href="#" className="text-white">
+            Library Manga
+          </a>
+        </div>
+
+        <ul className="hidden md:flex space-x-4">
+          <li>
+            <a href="#" className="hover:text-gray-400">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:text-gray-400">
+              About
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:text-gray-400">
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:text-gray-400">
+              Contact
+            </a>
+          </li>
+        </ul>
+
+        <div className="md:hidden">
+          <button id="menuButton" className="text-white focus:outline-none">
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
+            </svg>
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+}
--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,3 +1,13 @@
+import TopBar from '@/components/TopBar';
+import { Outlet } from 'react-router-dom';
+
 export const Root: React.FC = () => {
-  return <></>;
+  return (
+    <>
+      <TopBar />
+      <div className="m-16">
+        <Outlet />
+      </div>
+    </>
+  );
 };
