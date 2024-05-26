636e906f674e57700fb08fc30ebaae4e4553e467
Guilherme Alves <guilhermealvescoc@gmail.com>
2024-05-26 17:36:38 -0300

    feat: Add new function to search manga by name and adjust typing of other functions in API call file

--- src/api/mangaApi.ts
+++ src/api/mangaApi.ts
@@ -1,17 +1,23 @@
 import apiJikan from './axiosConfig';
-import { MangaDataType } from './models/manga.interface';
+import { MangaDataType, MangaType } from './models/manga.interface';
 
 async function getTopMangas(): Promise<MangaDataType> {
-  const response = await apiJikan.get<MangaDataType>('/top/manga?limit=10');
-  const topMangas = response.data;
+  const response = await apiJikan.get('/top/manga?limit=10');
+  const topMangas: MangaDataType = response.data;
   return topMangas;
 }
 
 async function mangaInfoLoader(mangaId: string | undefined) {
   const id = Number(mangaId);
-  const response = await apiJikan.get<MangaDataType>(`/manga/${id}/full`);
-  const mangaInfo = response.data.data;
+  const response = await apiJikan.get(`/manga/${id}/full`);
+  const mangaInfo: MangaType = response.data.data;
   return mangaInfo;
 }
 
-export { getTopMangas, mangaInfoLoader };
+async function mangaSearch(query: string) {
+  const response = await apiJikan.get(`/manga?q=${query}`);
+  const mangaInfo: MangaDataType = response.data;
+  return mangaInfo;
+}
+
+export { getTopMangas, mangaInfoLoader, mangaSearch };
