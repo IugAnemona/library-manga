93201ca80a8881468591c48e4bc1b122a845bf87
Guilherme Alves <guilhermealvescoc@gmail.com>
2024-05-26 17:40:27 -0300

    feat: add input component with debounce

---
+++ src/components/InputSearch/index.tsx
@@ -0,0 +1,23 @@
+import { ChangeEvent, useState } from 'react';
+import { Input } from '../ui/input';
+import useDebounce from '@/customHooks/useDebounce';
+
+interface InputSearchType {
+  value: string;
+  onChange: (...args: Array<string>) => void;
+}
+
+export function InputWithDebounce({ value, onChange }: InputSearchType) {
+  const [displayValue, setDisplayValue] = useState(value);
+  const debounceChange = useDebounce(onChange, 1500);
+
+  function handleChange(event: ChangeEvent<HTMLInputElement>) {
+    setDisplayValue(event.target.value);
+    debounceChange(event.target.value);
+  }
+  return (
+    <div className="max-w-96 m-auto">
+      <Input type="search" value={displayValue} onChange={handleChange} />
+    </div>
+  );
+}
